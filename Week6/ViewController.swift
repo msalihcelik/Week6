72c85258b115e43de298f8d2f10bc58b532c4674
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2021-10-20 14:24:30 +0300

    Initial Commit

---
+++ Week6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week6
+//
+//  Created by Mehmet Salih ÇELİK on 20.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
