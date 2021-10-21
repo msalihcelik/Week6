ff4848e69a74b07582fcd6ffbc6bd10ea29656ea
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2021-10-22 02:37:11 +0300

    add functions

--- Week6/ViewController.swift
+++ Week6/ViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tabBarController?.tabBar.tintColor = UIColor.darkGray
+        tabBarController?.tabBar.unselectedItemTintColor = UIColor.lightGray
+        tabBarController?.tabBar.items?[0].setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "WickedHalloween", size: 15)], for: .normal)
+        tabBarController?.tabBar.items?[0].image = UIImage(named: "icon11")
+        tabBarController?.tabBar.items?[0].selectedImage = UIImage(named: "icon11")
+        
+    
+        tabBarController?.tabBar.items?[1].setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "WickedHalloween", size: 15)], for: .normal)
+        
     }
-
+    
+    @IBAction func logInButtonTapped(_ sender: Any) {
+        
+        if (nameTextField.text == "" || passwordTextField.text == "") {
+            showAlertView()
+        } else {
+            performSegue(withIdentifier: "toSuccessVC", sender: nil)
+        }
+        
+    }
+    
+    public func showAlertView() {
+        let alert = UIAlertController(title: "Lütfen boş alan bırakmayınız!", message: "", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        self.present(alert, animated: true)
+    }
+    
 }
